96186fa81214631c2214dc1de41d50b01645f875
gegeupr <gegeupr@gmail.com>
2025-08-11 21:56:22 -0300

    feat: added final vercel.json config

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
-// Servindo todos os arquivos estáticos da pasta raiz
 app.use(express.static(path.join(__dirname, '')));
 
-// Endpoint para criar a sessão de checkout
 app.post('/api/create-checkout-session', async (req, res) => {
   const { priceId } = req.body;
 
@@ -42,12 +40,10 @@ app.post('/api/create-checkout-session', async (req, res) => {
   }
 });
 
-// Endpoint para verificar a assinatura (ainda não implementado)
 app.post('/api/verify-subscription', async (req, res) => {
   res.status(200).json({ message: 'Verificação em desenvolvimento.' });
 });
 
-// Iniciando o servidor
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
