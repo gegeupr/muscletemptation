478fa57767cb2f4f1d271b7886b08abcd1843b86
gegeupr <gegeupr@gmail.com>
2025-08-11 22:03:08 -0300

    feat: added final vercel.json config

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const Stripe = require('stripe');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Substitua com a sua Chave Secreta do Stripe
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
 
+// Servindo todos os arquivos estáticos (HTML, CSS, JS) da pasta raiz
 app.use(express.static(path.join(__dirname, '')));
 
+// Endpoint para criar a sessão de checkout
 app.post('/api/create-checkout-session', async (req, res) => {
   const { priceId } = req.body;
 
@@ -40,10 +43,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
   }
 });
 
-app.post('/api/verify-subscription', async (req, res) => {
-  res.status(200).json({ message: 'Verificação em desenvolvimento.' });
-});
-
+// Iniciando o servidor
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
